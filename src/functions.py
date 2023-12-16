82b2bd7a4fd284f19925fbfb9280501e355e0762
angelinakhalueva <sprite.spirit.t.me@gmail.com>
2023-12-17 00:00:03 +0300

    create func mask_card_number() and a test for it

--- src/functions.py
+++ src/functions.py
@@ -10,3 +10,14 @@ def get_all_transactions(filename: str) -> list:
     with open(filename) as file:
         json_data = json.load(file)
         return json_data
+
+
+def mask_card_number(card_number: str) -> str:
+    """
+    Создает маску-шаблон для скрытия номера карты.
+    :param card_number: Номер карты с указанием платежной системы.
+    :return: Маска номера карты по заданному шаблону.
+    """
+    return f"{card_number[:-16]}{card_number[-16:-12]} {card_number[-12:-10]}** **** {card_number[-4:]}"
+
+
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,1 +1,9 @@
 from src import functions as func
+
+
+def test_mask_card_number():
+    assert func.mask_card_number("Maestro 1913883747791351") == "Maestro 1913 88** **** 1351"
+    assert func.mask_card_number("Visa Platinum 3530191547567121") == "Visa Platinum 3530 19** **** 7121"
+    assert func.mask_card_number("МИР 2052809263194182") == "МИР 2052 80** **** 4182"
+    assert func.mask_card_number("Visa Platinum 3530191547567121") == "Visa Platinum 3530 19** **** 7121"
+
